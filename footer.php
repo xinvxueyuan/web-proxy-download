1445bcbfac8321476d221842979e200fedbd5afd
xinvxueyuan <xinvxueyuan@yeah.net>
2025-04-26 11:53:59 +0800

    版权信息

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="footer mt-auto py-3 bg-light text-center">
     <div class="container">
-        <span class="text-muted">&copy; 2024 简单代理下载. 保留所有权利.</span>
+        <span class="text-muted">&copy; 2025 简单代理下载. 保留所有权利.</span>
     </div>
 </footer>
